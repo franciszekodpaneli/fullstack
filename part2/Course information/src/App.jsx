25cc002947d4a54fe715b7a98c7d632563fe737b
Adam <vvedus@gmail.com>
2026-01-29 14:22:34 +0100

    C6

--- part2/Course information/src/App.jsx
+++ part2/Course information/src/App.jsx
@@ -1,56 +1,45 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.name} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
-  };
+  }
 
-  const Header = (props) => {
-    return <h1>{props.course.name}</h1>; // Show course name, not the entire object
-  };
-
-  const Part = (props) => {
-    return <p>{props.name} {props.exercises}</p>;
-  };
-
-  const Content = (props) => {
-    return (
-      <div>
-        {props.parts.map((part, index) => (
-          <Part key={index} name={part.name} exercises={part.exercises} />
-        ))}
-      </div>
-    );
-  };
-
-  const Total = (props) => {
-    const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-    return (
-      <p>
-        Number of exercises {totalExercises}
-      </p>
-    );
-  };
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />
 }
 
-export default App;
+export default App
